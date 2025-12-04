6e72faa8ad0245903db888dc3bf9d2fb3590f95f
VictorCruz <vc2022073903@virtual.upt.pe>
2025-12-04 00:40:19 -0500

    Add comprehensive unit tests to increase coverage and configure SonarCloud

--- Bank/Bank.Domain.Tests/BankAccountTests.cs
+++ Bank/Bank.Domain.Tests/BankAccountTests.cs
@@ -33,5 +33,82 @@ namespace Bank.Domain.Tests
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 15.00;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsNegative_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = -5.00;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Credit_WhenAmountIsNegative_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double creditAmount = -5.00;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Credit(creditAmount);
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldSetCustomerNameCorrectly()
+        {
+            string customerName = "Mr. Bryan Walton";
+            double balance = 11.99;
+            var account = new BankAccount(customerName, balance);
+
+            Assert.AreEqual(customerName, account.CustomerName);
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldSetBalanceCorrectly()
+        {
+            string customerName = "Mr. Bryan Walton";
+            double balance = 11.99;
+            var account = new BankAccount(customerName, balance);
+
+            Assert.AreEqual(balance, account.Balance, 0.001);
+        }
+
+        [TestMethod]
+        public void Debit_WithZeroAmount_ShouldNotChangeBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 0;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            Assert.AreEqual(beginningBalance, account.Balance, 0.001);
+        }
+
+        [TestMethod]
+        public void Credit_WithZeroAmount_ShouldNotChangeBalance()
+        {
+            double beginningBalance = 11.99;
+            double creditAmount = 0;
+            var account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Credit(creditAmount);
+
+            Assert.AreEqual(beginningBalance, account.Balance, 0.001);
+        }
     }
 }
